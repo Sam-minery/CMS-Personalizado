a62e911a3f6d66d6a3da723c2e985627c741c842
shervin hatami <hatamishervin13@gmail.com>
2025-09-30 16:33:41 +0200

    arreglo migraciones

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,49 +1,9 @@
-import * as migration_20250916_103935 from './20250916_103935';
-<<<<<<< HEAD
-import * as migration_20250929_163439 from './20250929_163439';
-import * as migration_20250929_170033 from './20250929_170033';
-=======
-import * as migration_20250925_120238 from './20250925_120238';
-import * as migration_20250925_124228 from './20250925_124228';
-import * as migration_20250929_141041 from './20250929_141041';
-import * as migration_20250930_085739 from './20250930_085739';
->>>>>>> origin/sam-branch
+import * as migration_20250930_142134_init from './20250930_142134_init';
 
 export const migrations = [
   {
-    up: migration_20250916_103935.up,
-    down: migration_20250916_103935.down,
-    name: '20250916_103935',
-  },
-  {
-<<<<<<< HEAD
-    up: migration_20250929_163439.up,
-    down: migration_20250929_163439.down,
-    name: '20250929_163439',
-  },
-  {
-    up: migration_20250929_170033.up,
-    down: migration_20250929_170033.down,
-    name: '20250929_170033'
-=======
-    up: migration_20250925_120238.up,
-    down: migration_20250925_120238.down,
-    name: '20250925_120238',
-  },
-  {
-    up: migration_20250925_124228.up,
-    down: migration_20250925_124228.down,
-    name: '20250925_124228',
-  },
-  {
-    up: migration_20250929_141041.up,
-    down: migration_20250929_141041.down,
-    name: '20250929_141041',
-  },
-  {
-    up: migration_20250930_085739.up,
-    down: migration_20250930_085739.down,
-    name: '20250930_085739'
->>>>>>> origin/sam-branch
+    up: migration_20250930_142134_init.up,
+    down: migration_20250930_142134_init.down,
+    name: '20250930_142134_init'
   },
 ];
